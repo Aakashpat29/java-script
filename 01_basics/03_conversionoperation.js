e29b7355a273a03898fe597ce191da3a86233c87
Aakashpat29 <patelaakash2904@gmail.com>
2025-08-02 10:03:00 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,34 @@
+// let score = 33
+// console.log(typeof score);
+
+
+
+
+// let score = "33"
+// console.log(typeof score);
+
+// //how to convert a string into number
+
+// let valueinNumber = Number(score)
+
+// console.log(typeof valueinNumber)
+// console.log(valueinNumber)
+
+
+
+let score = "33abc"
+console.log(typeof score)
+
+let valueinNumber = Number(score)
+console.log(typeof valueinNumber)
+console.log(valueinNumber)//it show NaN it means it is not a number(NaN) in case of null it show 0 or in case of undefined it show NaN
+
+// to convert the Boolean we use Boolean in place of Number.
+
+let sum ="Aakash"   //in case of 0 it show false for boolean otherwise it shoe true in conversion of boolean.if we convert string into boolean it show always true 
+console.log(typeof sum)
+console.log(sum)
+
+let isboolean = Boolean(sum)
+console.log(typeof isboolean)
+console.log(isboolean)
